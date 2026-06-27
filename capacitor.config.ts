08e953026cc0dad20d50be6e1cd3fd656323c97d
Lemonade Developer <developer@lemonadetycoon.local>
2026-06-28 00:03:10 +0700

    Change appId and bundle identifier to com.kieuducha.lemonadeclone

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.gamez0.lemonadetycoon',
+  appId: 'com.kieuducha.lemonadeclone',
   appName: 'Lemonade Tycoon',
   webDir: 'dist'
 };
